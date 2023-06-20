f9fba181c0fa019ac37e6f21652e2317a7a510b7
Sharath Thomas <sharathct22@gmail.com>
2023-06-20 01:13:31 +0100

    added a basic server

---
+++ python/src/toolbox/server/mount_api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+def mount_api(app: FastAPI):
+    @app.get("/api")
+    def read_root():
+        return {"Hello": "World"}
+
+    @app.get("/api/items/{item_id}")
+    def read_item(item_id: int, q: str = ""):
+        return {"item_id": item_id, "q": q}
+
+    return app
---
+++ python/src/toolbox/server/mount_frontend.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+def mount_frontend(app: FastAPI, react_build_dir: Path):
+    app.mount("/", StaticFiles(directory=react_build_dir, html=True), name="static")
+    app.mount("/static", StaticFiles(directory=(react_build_dir / "static") ), name="static")
+    return app
