1b71ad63c8aad9176dbf516d45e8cc4ba69c91cb
Sharath Thomas <sharathct22@gmail.com>
2023-08-02 12:45:05 +0100

    added terminal backend barebone

--- python/src/toolbox/api/terminal.py
+++ python/src/toolbox/api/terminal.py
@@ -1,7 +1,11 @@
 """APIs for implementing a websocket terminal."""
+import os
+import select
+import sys
+import termios
+import tty
 
 from fastapi import FastAPI, WebSocket
-from toolbox.core.terminal import TerminalModel
 
 
 def terminal_endpoints(app: FastAPI) -> FastAPI:
@@ -18,7 +22,23 @@ def terminal_endpoints(app: FastAPI) -> FastAPI:
     async def terminal(websocket: WebSocket) -> None:
         """Run a terminal."""
         await websocket.accept()
-        terminal = TerminalModel(websocket=websocket)
-        await terminal.run()
+        master, slave = os.openpty()
+        old_term_settings = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setraw(master)
+            while True:
+                rlist, _, _ = select.select([sys.stdin, master], [], [])
+                for src in rlist:
+                    if src == master:
+                        data = os.read(master, 1024)
+                        await websocket.send_text(data.decode())
+                    elif src == websocket:
+                        data = await websocket.receive_text()
+                        os.write(master, data.encode())
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_term_settings)
+            os.close(master)
+            os.close(slave)
+            await websocket.close()
 
     return app
