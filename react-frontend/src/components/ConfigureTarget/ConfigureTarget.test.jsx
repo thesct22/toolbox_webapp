8dc1dfa1f571b9177648334bcd403c12808f002a
Sharath Thomas <sharathct22@gmail.com>
2023-09-04 12:11:48 +0100

    js linting done

--- react-frontend/src/components/ConfigureTarget/ConfigureTarget.test.jsx
+++ react-frontend/src/components/ConfigureTarget/ConfigureTarget.test.jsx
@@ -4,33 +4,34 @@ import ConfigureTarget from './ConfigureTarget';
 
 // Mock the fetch function to simulate API calls
 global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        public_key: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAspgMUpVAX1MfBUnqG50m\nVdCdzPbf/yauINkSfCkQMLTLw6827FbCj0nTNnscYpoaovlGkt01AuwlRwQgdMee\nr3y2kaSwuJbMPqTOMss0OmRjGioQVgo2TDDATiB8nmhiLiZaO1MIP2NM1+dDbRm2\nCdqyPJ8zHBNR4F5yVrGA8fEbUk1NG9bWEt1fQqMuf3NpC2RZJDFA+ETE41fisz5R\n45mMtmIak1Swz7DQx2cTVUDw785YCKq6tVn8HKh2FC0F7Dfs5d4xLL5UlpQquFkJ\nxA3nP1ml+gPhTjhSzyhbyUuKprmWQb3+9ydBHTvVCnHcsnNFeA9H/9BdRCyXmG2Z\newIDAQAB\n-----END PUBLIC KEY-----\n',
-      }),
-    ok: true,
-  })
+	Promise.resolve({
+		json: () =>
+			Promise.resolve({
+				public_key:
+					'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAspgMUpVAX1MfBUnqG50m\nVdCdzPbf/yauINkSfCkQMLTLw6827FbCj0nTNnscYpoaovlGkt01AuwlRwQgdMee\nr3y2kaSwuJbMPqTOMss0OmRjGioQVgo2TDDATiB8nmhiLiZaO1MIP2NM1+dDbRm2\nCdqyPJ8zHBNR4F5yVrGA8fEbUk1NG9bWEt1fQqMuf3NpC2RZJDFA+ETE41fisz5R\n45mMtmIak1Swz7DQx2cTVUDw785YCKq6tVn8HKh2FC0F7Dfs5d4xLL5UlpQquFkJ\nxA3nP1ml+gPhTjhSzyhbyUuKprmWQb3+9ydBHTvVCnHcsnNFeA9H/9BdRCyXmG2Z\newIDAQAB\n-----END PUBLIC KEY-----\n',
+			}),
+		ok: true,
+	})
 );
 
 test('renders the ConfigureTarget component', () => {
-  const { getByLabelText, getByText } = render(<ConfigureTarget />);
-  
-  // Check if important elements are rendered
-  const usernameInput = getByLabelText('Username');
-  const passwordInput = getByLabelText('Password');
-  const hostsInput = getByLabelText('Hostnames/IP Addresses');
-  const configureButton = getByText('Configure');
+	const { getByLabelText, getByText } = render(<ConfigureTarget />);
 
-  expect(usernameInput).toBeInTheDocument();
-  expect(passwordInput).toBeInTheDocument();
-  expect(hostsInput).toBeInTheDocument();
-  expect(configureButton).toBeInTheDocument();
+	// Check if important elements are rendered
+	const usernameInput = getByLabelText('Username');
+	const passwordInput = getByLabelText('Password');
+	const hostsInput = getByLabelText('Hostnames/IP Addresses');
+	const configureButton = getByText('Configure');
+
+	expect(usernameInput).toBeInTheDocument();
+	expect(passwordInput).toBeInTheDocument();
+	expect(hostsInput).toBeInTheDocument();
+	expect(configureButton).toBeInTheDocument();
 });
 
 // test('submits form data when Configure button is clicked', async () => {
 //   const { getByLabelText, getByText } = render(<ConfigureTarget />);
-  
+
 //   const usernameInput = getByLabelText('Username');
 //   const passwordInput = getByLabelText('Password');
 //   const hostsInput = getByLabelText('Hostnames/IP Addresses');
